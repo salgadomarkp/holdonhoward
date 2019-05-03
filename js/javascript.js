9afc7ffbab684a7b5792d3bf42d539c2133c6af0
Mark Salgado <salgadomarkp@gmail.com>
2019-05-03 00:31:51 -0400

    added content and photos

--- js/javascript.js
+++ js/javascript.js
@@ -4,8 +4,22 @@ $( document ).ready(function() {
 
     console.log("Document is ready");
 
+    //Clicking ANY dropdown button
     $( ".dropdown__button").click(function() {
-        $( ".dropdown__list").toggleClass("hidden");
+        //If we clicked on an open dropdown
+        if($(this).siblings(".dropdown__list").hasClass("hidden") == false)
+        {
+            //Hide it
+            $(this).siblings(".dropdown__list").addClass("hidden");
+        }
+        //Otherwise
+        else{
+            //Hide all of the list items
+            $(".dropdown__list").addClass("hidden");
+    
+            //remove the hidden class from the clicked drop down
+            $(this).siblings(".dropdown__list").removeClass("hidden");
+        }
     });
 
 });
